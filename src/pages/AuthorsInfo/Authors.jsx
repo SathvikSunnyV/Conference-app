cc798707ce804be486620a10d64ab3fc3916b41f
SathvikSunnyV <ndtoughr@gmail.com>
2026-05-07 09:38:53 +0530

    animations and stylings

--- src/pages/AuthorsInfo/Authors.jsx
+++ src/pages/AuthorsInfo/Authors.jsx
@@ -1,9 +1,42 @@
+import React, { useEffect, useRef, useState } from "react";
 import "./Authors.css";
+
+function AnimatedSection({ children, className = "" }) {
+  const ref = useRef(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+        }
+      },
+      {
+        threshold: 0.15,
+        rootMargin: "0px 0px -60px 0px",
+      }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div
+      ref={ref}
+      className={`fade-section ${visible ? "visible" : ""} ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
 
 function Authors() {
   return (
     <div className="authors-container">
-      
+
       <div className="title-wrapper">
         <div className="title-box">
           <h2>Information For Authors</h2>
@@ -12,13 +45,15 @@ function Authors() {
 
       <div className="authors-content">
 
-        <h3>Submission Policies</h3>
+        <AnimatedSection>
+          <h3>Submission Policies</h3>
 
-        <p>
-          The authors of ASSIC 2026 should pay attention to the following:
-        </p>
+          <p>
+            The authors of ASSIC 2026 should pay attention to the following:
+          </p>
+        </AnimatedSection>
 
-        <div className="policy-list">
+        <AnimatedSection className="policy-list">
           <p>
             * Original, previously unpublished papers, and not simultaneously
             submitted to another journal or conference, with scientific and
@@ -41,9 +76,9 @@ function Authors() {
             * Papers have to be formatted in the conference proceedings layout
             on A4 paper according to IEEE Template as follows:
           </p>
-        </div>
+        </AnimatedSection>
 
-        <div className="template-links">
+        <AnimatedSection className="template-links">
           <p>
             WORD:
             <a
@@ -51,7 +86,7 @@ function Authors() {
               target="_blank"
               rel="noreferrer"
             >
-              http://www.ieee.info/IEEE_template.docx
+              Download Template
             </a>
           </p>
 
@@ -62,39 +97,38 @@ function Authors() {
               target="_blank"
               rel="noreferrer"
             >
-              http://www.ieee.info/IEEE_latex.zip
+              Download Template
             </a>
           </p>
-        </div>
+        </AnimatedSection>
 
-        <h3 className="submission-title">Paper Submission</h3>
+        <AnimatedSection>
+          <h3 className="submission-title">Paper Submission</h3>
 
-        <p className="submission-text">
-          Upload your paper{" "}
-          <a href="/">here</a>
-        </p>
+          <p className="submission-text">
+            Upload your paper <a href="/">here</a>
+          </p>
+        </AnimatedSection>
 
-        <h3 className="camera-title">Camera-Ready Submission</h3>
+        <AnimatedSection>
+          <h3 className="camera-title">Camera-Ready Submission</h3>
 
-        <p>
-          Upon acceptance, at least one of the authors listed in the paper must
-          attend the conference and present the paper according to the schedule
-          of the conference. In the event that the presenter is unable to attend
-          the conference for any reasons, your accepted paper will not be
-          considered for submission of the proceeding.
-        </p>
+          <p>
+            Upon acceptance, at least one of the authors listed in the paper must
+            attend the conference and present the paper according to the schedule
+            of the conference.
+          </p>
 
-        <p>
-          At least one of the authors listed on the paper must register for the
-          ASSIC 2026 to upload the final manuscript. The final manuscript must
-          be prepared based on reviewers’ comments and according to the final
-          paper format.
-        </p>
+          <p>
+            At least one of the authors listed on the paper must register for the
+            ASSIC 2026 to upload the final manuscript.
+          </p>
 
-        <p>
-          Only paper that have been corrected according to reviewer(s)
-          suggestion will be considered to be included in the final proceedings.
-        </p>
+          <p>
+            Only paper that have been corrected according to reviewer(s)
+            suggestion will be considered to be included in the final proceedings.
+          </p>
+        </AnimatedSection>
 
       </div>
     </div>
